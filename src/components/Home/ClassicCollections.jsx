00e8d971cf261f11bac6373d4ae144f383eba515
Tife <boluwatife1904@gmail.com>
2025-10-08 14:22:23 +0100

    added more images for the collections marquee and cleaned up the content

--- src/components/Home/ClassicCollections.jsx
+++ src/components/Home/ClassicCollections.jsx
@@ -4,9 +4,149 @@ import rolls from "@/assets/rolls_collect.webp";
 import ford from "@/assets/ford_collect.webp";
 import lambo from "@/assets/lambo_collect.webp";
 import ferrari from "@/assets/ferrari_collect.webp";
+import porsche from "@/assets/porsche_collect.webp";
+import jaguar from "@/assets/jaguar_collect.webp";
+import shelby from "@/assets/shelby_collect.webp";
+import mercedes from "@/assets/mercedes_collect.webp";
+import mclaren from "@/assets/mclaren_collect.webp";
+import aston from "@/assets/aston_collect.webp";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ParallaxContainer from "../reusables/ParallaxContainer";
+
+const list1 = [
+  {
+    img: ferrari,
+    heading: "ferrari 250 GTO",
+    pTags: (
+      <>
+        <p>era: 1960s legend </p>
+        <p>power: 3.0l v12 engine</p>
+        <p>rarity: only 36 ever made </p>
+        <p>nickname: "the million-dollar stallion"</p>
+      </>
+    ),
+  },
+  {
+    img: rolls,
+    heading: "rolls-royce phantom v",
+    pTags: (
+      <>
+        <p>era: 1950s ultimate luxury </p>
+        <p>power: 6.2l v8</p>
+        <p>exclusivity: owned by royals & rockstars </p>
+        <p>nickname: "the silent king"</p>
+      </>
+    ),
+  },
+
+  {
+    img: lambo,
+    heading: "lamborghini miura p400 sv",
+    pTags: (
+      <>
+        <p>era: 1960s birth of the supercar </p>
+        <p>power: 385 hp, 4.01 v12 </p>
+        <p>iconic feature: first mid-engine supercar </p>
+        <p>nickname: "the raging bull"</p>
+      </>
+    ),
+  },
+
+  {
+    img: ford,
+    heading: "ford gt40",
+    pTags: (
+      <>
+        <p>era: 1960s racing dominance </p>
+        <p>power: 7.0l v8 engine producing around 485 hp</p>
+        <p>
+          defining feature: four-time le mans champion, a symbol of endurance
+          and relentless pursuit{" "}
+        </p>
+        <p>nickname: "the le mans conqueror"</p>
+      </>
+    ),
+  },
+  {
+    img: porsche,
+    heading: "porsche 911 turbo 930",
+    pTags: (
+      <>
+        <p>era: 1970s turbo revolution</p>
+        <p>power: 3.3l turbocharged flat-6</p>
+        <p>notoriety: the "widowmaker"</p>
+        <p>race pedigree: le mans provenace</p>
+      </>
+    ),
+  },
+];
+
+const list2 = [
+  {
+    img: jaguar,
+    heading: "jaguar e-type series 1",
+    pTags: (
+      <>
+        <p>era: 1960s automotive art</p>
+        <p>power: 3.8l straight-six </p>
+        <p>nickname: "the most beautiful car in the world" – enzo ferrari</p>
+        <p>top speed: 150 mph record-breaker</p>
+      </>
+    ),
+  },
+  {
+    img: shelby,
+    heading: "shelby cobra 427",
+    pTags: (
+      <>
+        <p>era: late 1960s muscle and agility</p>
+        <p>power: 7.0l v8 engine delivering over 400 hp</p>
+        <p>
+          defining feature: raw, aggressive design paired with nimble handling{" "}
+        </p>
+        <p>top speed: "the american wildcat" </p>
+      </>
+    ),
+  },
+  {
+    img: mercedes,
+    heading: "mercedes 300sl gullwing",
+    pTags: (
+      <>
+        <p>era: 1950s jet-age engineering</p>
+        <p>power: 3.0l straight-six</p>
+        <p>defining feature: iconic gullwing doors</p>
+        <p>status: first supercar ever</p>
+      </>
+    ),
+  },
+  {
+    img: mclaren,
+    heading: "mclaren f1",
+    pTags: (
+      <>
+        <p>era: 1990s hypercar revolution</p>
+        <p>power: 6.1l v12 by bmw, 627 hp</p>
+        <p>defining feature: gold-plated engine bay for heat insulation</p>
+        <p>top speed: 240.1 mph (world’s fastest car for a decade)</p>
+        <p>nickname: "the three-seater spaceship"</p>
+      </>
+    ),
+  },
+  {
+    img: aston,
+    heading: "aston martin db5",
+    pTags: (
+      <>
+        <p>era: 1960s british luxury meets performance</p>
+        <p>power: 4.0l straight-six</p>
+        <p>fame: james bond’s 007 car </p>
+        <p>craftmanship: handbuilt interior</p>
+      </>
+    ),
+  },
+];
 
 export default function ClassicCollections() {
   return (
@@ -34,110 +174,18 @@ export default function ClassicCollections() {
       </div>
       {/* Container for marquees */}
       <div className="flex flex-col gap-[112px] mt-[200px] -1024:mt-[150px]">
-        <CollectionMarquee />
-        <CollectionMarquee reverse={true} />
+        <CollectionMarquee list={list1} />
+        <CollectionMarquee reverse={true} list={list2} />
       </div>
     </section>
   );
 }
 
-const list1 = [
-  {
-    img: ferrari,
-    heading: "ferrari 250 GTO",
-    era: "1960s legend",
-    power: " 3.0l v12 engine",
-    exclusive: "only 36 ever made",
-    nickname: `"the million-dollar stallion"`,
-  },
-  {
-    img: rolls,
-    heading: "rolls-royce phantom v",
-    era: "1950s ultimate luxury",
-    power: "6.2l v8",
-    exclusive: "owned by royals & rockstars",
-    nickname: `"the silent king"`,
-  },
-
-  {
-    img: lambo,
-    heading: "lamborghini miura p400 sv",
-    era: "1960s birth of the supercar",
-    power: "385 hp, 4.0l v12",
-    exclusive: "first mid-engine supercar",
-    nickname: `"the raging bull"`,
-  },
-
-  {
-    img: ford,
-    heading: "ford gt40",
-    era: "1960s racing dominance",
-    power: "7.0l v8 engine producing around 485 hp",
-    exclusive:
-      "four-time le mans champion, a symbol of endurance and relentless pursuit",
-    nickname: `"the le mans conqueror"`,
-  },
-];
-
-function CollectionMarquee({ reverse }) {
+function CollectionMarquee({ reverse, list }) {
   const row1 = useRef(null);
   const row2 = useRef(null);
 
-  useGSAP(() => {
-    // gsap
-    //   .timeline({ repeat: -1 })
-    //   .to(row1.current, {
-    //     // x: reverse ? "0%" : "-100%",
-    //     keyframes: {
-    //       "0%": { x: reverse ? "-100%" : "0%", ease: "none" },
-    //       "100%": { x: reverse ? "0%" : "-100%", ease: "none" },
-    //     },
-    //     duration: 17,
-    //     ease: "none",
-    //   })
-    //   .to(
-    //     row2.current,
-    //     {
-    //       // x: reverse ? "0%" : "-100%",
-    //       keyframes: {
-    //         "0%": { x: reverse ? "-100%" : "0%", ease: "none" },
-    //         "100%": { x: reverse ? "0%" : "-100%", ease: "none" },
-    //       },
-    //       duration: 17,
-    //       ease: "none",
-    //     },
-    //     "<"
-    //   );
-  }, []);
-
   const parent = useRef(null);
-
-  // function handleEnter() {
-  //   const allMarquees = parent.current.querySelectorAll(".marq");
-
-  //   allMarquees.forEach((marquee) => {
-  //     gsap.to(marquee, {
-  //       duration: 1.5,
-  //       ease: "power2.inOut",
-  //       onUpdate: () => {
-  //         marquee.style.animationDuration = "40s";
-  //       },
-  //     });
-  //   });
-  // }
-  // function handleLeave() {
-  //   const allMarquees = parent.current.querySelectorAll(".marq");
-
-  //   allMarquees.forEach((marquee) => {
-  //     gsap.to(marquee, {
-  //       duration: 1.5,
-  //       ease: "power2.inOut",
-  //       onUpdate: () => {
-  //         marquee.style.animationDuration = "4s";
-  //       },
-  //     });
-  //   });
-  // }
 
   return (
     <div className="overflow-hidden flex w-full marquee-parent" ref={parent}>
@@ -147,7 +195,7 @@ function CollectionMarquee({ reverse }) {
           reverse ? "reverse-marquee translate-x-[-100%]" : "marquee"
         } `}
       >
-        {list1.map((car) => {
+        {list.map((car) => {
           return <CollectionCard {...car} />;
         })}
       </div>
@@ -157,7 +205,7 @@ function CollectionMarquee({ reverse }) {
           reverse ? "reverse-marquee translate-x-[-100%]" : "marquee"
         } `}
       >
-        {list1.map((car) => {
+        {list.map((car) => {
           return <CollectionCard {...car} />;
         })}
       </div>
@@ -165,7 +213,15 @@ function CollectionMarquee({ reverse }) {
   );
 }
 
-function CollectionCard({ img, era, heading, power, exclusive, nickname }) {
+function CollectionCard({
+  img,
+  era,
+  heading,
+  power,
+  exclusive,
+  nickname,
+  pTags,
+}) {
   return (
     <div className="w-[460px] -1024:max-w-[300px] flex flex-col items-start justify-start">
       {/* Image */}
@@ -179,12 +235,35 @@ function CollectionCard({ img, era, heading, power, exclusive, nickname }) {
       <h3 className="mt-[15px] card-heading">{heading}</h3>
 
       {/* Container for facts */}
-      <div className="tracking-tigheter font-medium">
-        <p className="mt-[16px]">era: {era}</p>
-        <p className="">power: {power}</p>
-        <p className="">exclusivity: {exclusive}</p>
-        <p className="">nickname: {nickname}</p>
-      </div>
+      <div className="tracking-tigheter font-medium pt-[16px]">{pTags}</div>
     </div>
   );
 }
+
+// Gsap marquee
+// useGSAP(() => {
+//   gsap
+//     .timeline({ repeat: -1 })
+//     .to(row1.current, {
+//       // x: reverse ? "0%" : "-100%",
+//       keyframes: {
+//         "0%": { x: reverse ? "-100%" : "0%", ease: "none" },
+//         "100%": { x: reverse ? "0%" : "-100%", ease: "none" },
+//       },
+//       duration: 17,
+//       ease: "none",
+//     })
+//     .to(
+//       row2.current,
+//       {
+//         // x: reverse ? "0%" : "-100%",
+//         keyframes: {
+//           "0%": { x: reverse ? "-100%" : "0%", ease: "none" },
+//           "100%": { x: reverse ? "0%" : "-100%", ease: "none" },
+//         },
+//         duration: 17,
+//         ease: "none",
+//       },
+//       "<"
+//     );
+// }, []);
